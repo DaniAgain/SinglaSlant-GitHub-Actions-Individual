2cb6bdeac01becf9c46b854efe5e185a445f1570
DaniAgain <115194305+DaniAgain@users.noreply.github.com>
2023-10-06 04:21:14 +0200

    Initial commit

---
+++ src/main/java/com/example/singlaslantgithubactions/SinglaSlantGitHubActionsApplication.java
@@ -0,0 +1,16 @@
+package com.example.singlaslantgithubactions;
+
+import com.example.singlaslantgithubactions.Model.CoinFlip;
+import com.example.singlaslantgithubactions.Model.RoundResult;
+import com.example.singlaslantgithubactions.Services.Game;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinglaSlantGitHubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinglaSlantGitHubActionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/singlaslantgithubactions/SinglaSlantGitHubActionsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.singlaslantgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinglaSlantGitHubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
